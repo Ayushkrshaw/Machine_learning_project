88e38b88581d059e026c576b62c1540a2ed5134c
Ayush <ayush2106kumarshaw@gmail.com>
2024-01-04 00:51:56 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
-
 #Declaring variables for setup functions
-
-
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
-AUTHOR="AYUSH"
-DESCRIPTION="This is a first project"
-PACKAGES=["housing"]
+VERSION="0.0.3"
+AUTHOR="Ayush"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 """Description: This function is going to return list of requirement mention in requirements.txt file
@@ -17,19 +14,20 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 """
 def get_requirements_list()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
+
 
 
 
 setup(
-    name=PROJECT_NAME,
-    version=VERSION,
-    author=AUTHOR,
-    description=DESCRIPTION,
-    packages=PACKAGES,
-    install_requires=get_requirements_list()
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
 )
-
-
+"""find_packages()->returns all the folders that contain __init__.py file"""
+"""-e . is used to download current files(housing_predictor) and its dependencies"""
 if __name__=="__main__":
     print(get_requirements_list())
